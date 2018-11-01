b838a34b34670e5864c7adef13913e4dfde37e0c
Fedor Fedoseev <fedor.fedoseev@tum.de>
2018-11-01 04:24:15 +0100

    FIFO

--- src/server/app/FIFO.java
+++ src/server/app/FIFO.java
@@ -1,4 +1,23 @@
 package server.app;
 
+import protocol.IMessage;
+
+import java.util.LinkedHashSet;
+
 public class FIFO implements ICacheDisplacementStrategy {
+  private LinkedHashSet<IMessage.K> registry = new LinkedHashSet<>(1000);
+
+  @Override
+  public IMessage.K evict() {
+    IMessage.K k = registry.iterator().next();
+    registry.iterator().remove();
+    registry.remove(k);
+    return k;
+  }
+
+  @Override
+  public void register(IMessage.K key) {
+    registry.remove(key);
+    registry.add(key);
+  }
 }
