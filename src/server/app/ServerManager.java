bb4b469be141671f602bff17d4a7293e5b1f62a6
Tuan Anh Ma <anh.ma@tum.de>
2018-11-02 17:17:45 +0100

    add echo server code

--- src/server/app/ServerManager.java
+++ src/server/app/ServerManager.java
@@ -1,10 +1,128 @@
 package server.app;
 
+import java.net.BindException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.io.IOException;
 
-public class ServerManager {
 
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import server.api.ClientConnection;
+
+/**
+ * Represents a simple Echo Server implementation.
+ */
+public class ServerManager extends Thread {
+
+    private static Logger logger = LogManager.getLogger(ClientConnection.class);
+
+    private int port;
+    private ServerSocket serverSocket;
+    private boolean running;
+
+    /**
+     * Constructs a (Echo-) Server object which listens to connection attempts
+     * at the given port.
+     *
+     * @param port a port number which the Server is listening to in order to
+     * 		establish a socket connection to a client. The port number should
+     * 		reside in the range of dynamic ports, i.e 49152 – 65535.
+     */
+    public ServerManager(int port){
+        this.port = port;
+    }
+
+    /**
+     * Initializes and starts the server.
+     * Loops until the the server should be closed.
+     */
+    public void run() {
+
+        running = initializeServer();
+
+        if(serverSocket != null) {
+            while(isRunning()){
+                try {
+                    Socket client = serverSocket.accept();
+                    ClientConnection connection =
+                            new ClientConnection(client);
+                    new Thread(connection).start();
+
+                    logger.info("Connected to "
+                            + client.getInetAddress().getHostName()
+                            +  " on port " + client.getPort());
+                } catch (IOException e) {
+                    logger.error("Error! " +
+                            "Unable to establish connection. \n", e);
+                }
+            }
+        }
+        logger.info("Server stopped.");
+    }
+
+    private boolean isRunning() {
+        return this.running;
+    }
+
+    /**
+     * Stops the server insofar that it won't listen at the given port any more.
+     */
+    public void stopServer(){
+        running = false;
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            logger.error("Error! " +
+                    "Unable to close socket on port: " + port, e);
+        }
+    }
+
+    private boolean initializeServer() {
+        logger.info("Initialize server ...");
+        try {
+            serverSocket = new ServerSocket(port);
+            logger.info("Server listening on port: "
+                    + serverSocket.getLocalPort());
+            return true;
+
+        } catch (IOException e) {
+            logger.error("Error! Cannot open server socket:");
+            if(e instanceof BindException){
+                logger.error("Port " + port + " is already bound!");
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Main entry point for the echo server application.
+     * @param args contains the port number at args[0].
+     */
     public static void main(String[] args) {
-        KVServer server = new KVServer(50000, 10, "FIFO" );
+        try {
+            /*TODO adapt logging with LogSetup
+            new LogSetup("logs/server.log", Level.ALL);*/
+
+            if(args.length != 1) {
+                System.out.println("Error! Invalid number of arguments!");
+                System.out.println("Usage: Server <port>!");
+            } else {
+                int port = Integer.parseInt(args[0]);
+                new ServerManager(port).start();
+            }
+        }
+        /* TODO adapt logging with LogSetup
+        catch (IOException e) {
+            System.out.println("Error! Unable to initialize logger!");
+            e.printStackTrace();
+            System.exit(1);
+        } */
+        catch (NumberFormatException nfe) {
+            System.out.println("Error! Invalid argument <port>! Not a number!");
+            System.out.println("Usage: Server <port>!");
+            System.exit(1);
+        }
     }
 }
