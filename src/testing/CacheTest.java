b999e2ec0bc8f8ce871124315550f7b87ad017a2
Fedor Fedoseev <fedor.fedoseev@tum.de>
2018-11-05 18:13:48 +0100

    init cache displacement tests

--- src/testing/CacheTest.java
+++ src/testing/CacheTest.java
@@ -9,6 +9,7 @@ import server.storage.CacheManager;
 import server.storage.cache.CacheDisplacementStrategy;
 import server.storage.cache.FIFO;
 
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -17,34 +18,62 @@ public class CacheTest extends TestCase {
   private class TestClient extends Thread {
     private final int from;
     private final int to;
+    private final Method updateCache;
 
     public TestClient(int from, int to, Method updateCache) {
       this.from = from;
       this.to = to;
+      this.updateCache = updateCache;
     }
 
     public void run() {
       for (int i = from; i < to; i++) {
+        K key = new K(Integer.toString(i).getBytes());
+        String valStr = "test" + Integer.toString(i);
+        V val = new V(valStr.getBytes());
+        try {
+          this.updateCache.invoke(key, val);
+        } catch (IllegalAccessException e) {
+          e.printStackTrace();
+        } catch (InvocationTargetException e) {
+          e.printStackTrace();
+        }
       }
     }
   }
 
-  @Test
-  public void testFIFO() throws NoSuchMethodException {
-    CacheManager cm = new CacheManager(100, CacheDisplacementStrategy.FIFO);
+  private void testCache(int capacity, CacheDisplacementStrategy strategy) throws NoSuchMethodException {
+    CacheManager cm = new CacheManager(capacity, strategy);
 
     Class<? extends CacheManager> cl = cm.getClass();
     Method updateCache = cl.getDeclaredMethod("updateCache", K.class, V.class);
     updateCache.setAccessible(true);
 
-    updateCache.invoke();
+    int keysPerClient = 10;
+    for (int i = 0; i < cm.getCacheCapacity() / keysPerClient; i++) {
+      new TestClient(i * keysPerClient, (i + 1) * keysPerClient, updateCache).start();
+    }
+
+    int to = cm.getCacheCapacity() / keysPerClient;
+    to *= keysPerClient;
+    for (int i = 0; i < to; i++) {
+      K key = new K(Integer.toString(i).getBytes());
+      assertTrue(cm.getCache().contains(key));
+    }
   }
 
   @Test
-  public void testLRU() {
+  public void testFIFO() throws NoSuchMethodException {
+    testCache(1000, CacheDisplacementStrategy.FIFO);
   }
 
   @Test
-  public void testLFU() {
+  public void testLRU() throws NoSuchMethodException {
+    testCache(1000, CacheDisplacementStrategy.LRU);
+  }
+
+  @Test
+  public void testLFU() throws NoSuchMethodException {
+    testCache(1000, CacheDisplacementStrategy.LFU);
   }
 }
