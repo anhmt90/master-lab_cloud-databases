c2bfd82fc5542ed2bf0950ab614c8ae00604a10e
Fedor Fedoseev <fedor.fedoseev@tum.de>
2018-11-17 11:02:21 +0100

    md5 hashing from string to hex string

---
+++ src/util/HashUtils.java
@@ -0,0 +1,30 @@
+package util;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class HashUtils {
+  private static MessageDigest md5;
+
+  static {
+    try {
+      md5 = MessageDigest.getInstance("MD5");
+    } catch (NoSuchAlgorithmException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public static String getHash(String s) {
+    byte[] digest = md5.digest(s.getBytes(StandardCharsets.UTF_8));
+    StringBuffer sb = new StringBuffer();
+    for (byte b: digest) {
+      sb.append(Integer.toHexString((b & 0xFF) | 0x100).substring(1, 3));
+    }
+    while (sb.length() < 32) {
+      sb.insert(0, '0');
+    }
+
+    return sb.toString();
+  }
+}
