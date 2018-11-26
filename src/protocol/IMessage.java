a6094e48b87754b2a079cbb793401f0486bb4850
Tuan Anh Ma <anh.ma@tum.de>
2018-11-27 00:47:51 +0100

    minor changes

--- src/protocol/IMessage.java
+++ src/protocol/IMessage.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import ecs.Metadata;
 
 public interface IMessage extends Serializable {
-    public static final int MAX_MESSAGE_LENGTH = 2 + 20 + 1024 * 120;
+    public static final int MAX_MESSAGE_LENGTH = 1 + 3 + 16 + 1024 * 120;
     enum Status {
         GET(0x01),            /* Get - request */
         GET_ERROR(0x02),        /* requested tuple (i.e. value) not found */
